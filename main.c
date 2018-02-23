8c9e663b00299d90006edc0b8591ce531145afe2
ChunHLi <chunhli@cs.stonybrook.edu>
2018-02-22 20:38:07 -0800

    Did alot of research, made struct and array, ready to do this tomorrow

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "helper.h"
+#include <inttypes.h>
 
 int main(int argc, char** argv) {
     if (*(argv + 1) == NULL) {
@@ -12,8 +13,30 @@ int main(int argc, char** argv) {
      * You code goes below. Do not modify provided parts
      * of the code.
      */
+	/*
+	 * uint64_t orders the bits from highest to lowest bit
+	 * the ram orders the bytes from lowest to highest byte
+	 * size is in words
+	 */
+	
+	typedef struct block{ 
+		uint64_t header;
+		uint64_t payload[124]; //(1024-16-16)/8 max size of ram		
+		uint64_t footer;
+	} block;
 
-
+	block ramArray[32]; 	
+	/*
+	int i;
+	for (i=0;i<512;i++){
+		if (i % 8 == 0){
+			printf("\n");
+		}
+		printf("%02x",((uint8_t*)ram)[i]);
+	}
+	*/
+	//printf("%"PRIu64"\n",((uint64_t*)ram)[0]);
+	//printf("%"PRIu64"\n",((uint64_t*)ram)[1]);
     /*
      * Do not modify code below.
      */
